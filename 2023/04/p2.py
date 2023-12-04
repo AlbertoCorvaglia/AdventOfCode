607b6c2cd5702c148ec3bb6c4d7785cb0e715e87
alberto <albertocorvaglia8@gmail.com>
2023-12-04 09:04:34 +0100

    day 4 part 2

--- 2023/04/p2.py
+++ 2023/04/p2.py
@@ -1,21 +1,16 @@
 inputFile = open("input.txt","r")
-nCards = 0
 
-print(cards)
-for line in inputFile:
-    cards.append(line)
-
-# for i in range(len(cards)):
-#     card = cards[i]
-    
-#     winningNums = card.strip().split(": ")[1].split(" | ")[0].split(" ")
-#     print(winningNums)
-#     playedNums = card.strip().split(": ")[1].split(" | ")[1].split(" ")
-
-#     nVinti = 0
-#     for num in winningNums:
-#         if  num.isdigit() and num in playedNums:
-#             nVinti += playedNums.count(num)
-
-
-# print(cards)
+cards = inputFile.readlines()
+nCards = [[1]*len(cards)][0]
+for i in range(len(cards)):
+    card = cards[i]
+    winningNums = card.strip().split(": ")[1].split(" | ")[0].split(" ")
+    playedNums = card.strip().split(": ")[1].split(" | ")[1].split(" ")
+    nVinti = 0
+    for num in winningNums:
+        if  num.isdigit() and num in playedNums:
+            nVinti += playedNums.count(num)
+    for j in range(nVinti):
+        nCards[i+j+1]+=1*nCards[i]
+    nVinti = 0
+print(sum(nCards))
